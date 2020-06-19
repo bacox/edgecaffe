b7b5664aa49e350c69f2f9fcc9be1370e8b9d133
bacox <b.a.cox@student.tudelft.nl>
2020-06-20 00:08:02 +0200

    Update script

--- scripts/copy_network_to_project.sh
+++ scripts/copy_network_to_project.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
+cp /opt/edgecaffe/networks/AgeNet/age_net.caffemodel ./networks/AgeNet/age_net.caffemodel
+cp -rf /opt/edgecaffe/networks/AgeNet/age_net_partials ./networks/AgeNet/age_net_partials
 
-cp /opt/networks/AgeNet/age_net.caffemodel ./networks/AgeNet/age_net.caffemodel
-cp -rf /opt/networks/AgeNet/age_net_partials ./networks/AgeNet/age_net_partials
-
-cp /opt/networks/FaceNet/face_full_conv.caffemodel ./networks/FaceNet/face_full_conv.caffemodel
-cp -rf /opt/networks/FaceNet/face_full_conv_partials ./networks/FaceNet/face_full_conv_partials
+cp /opt/edgecaffe/networks/FaceNet/face_full_conv.caffemodel ./networks/FaceNet/face_full_conv.caffemodel
+cp -rf /opt/edgecaffe/networks/FaceNet/face_full_conv_partials ./networks/FaceNet/face_full_conv_partials
