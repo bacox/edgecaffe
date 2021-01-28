d1518e6a04cc966f54377e34f8b42989b067394c
bacox <bartcox93@gmail.com>
2021-01-28 18:14:05 +0100

    Add missing dependencies to install file

--- install.sh
+++ install.sh
@@ -1,21 +1,23 @@
 
 sudo apt-get update
 # Other dependencies
-sudo apt-get install -y  build-essential libssl-dev wget gcc-8 g++-8 libopenblas-dev libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev libboost-all-dev protobuf-compiler libgflags-dev libgoogle-glog-dev liblmdb-dev python3-dev libatlas-base-dev doxygen libyaml-cpp-dev python3-pip
+sudo apt-get install -y  build-essential libssl-dev wget gcc-8 g++-8 libopenblas-dev libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev libboost-all-dev protobuf-compiler libgflags-dev libgoogle-glog-dev liblmdb-dev python3-dev libatlas-base-dev doxygen libyaml-cpp-dev python3-pip cgroup-bin
 sudo apt-get install -y --no-install-recommends libboost-all-dev
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 80 --slave /usr/bin/g++ g++ /usr/bin/g++-8 --slave /usr/bin/gcov gcov /usr/bin/gcov-8
 sudo update-alternatives --config gcc
-pip3 install protobuf
+pip3 install protobuf numpy pyyaml
 
+# Install cmake 3.19
 wget https://cmake.org/files/v3.19/cmake-3.19.3.tar.gz
-
 tar -xf ./cmake-3.19.3.tar.gz
 cd cmake-3.19.3
 ./bootstrap
 make -j2
 sudo make install
 rm -rf cmake-3.19.3
+cd ..
 
+# Generate probobuf files
 protoc caffe/src/caffe/proto/caffe.proto --cpp_out=.
 mkdir caffe/include/caffe/proto
 mv caffe/src/caffe/proto/caffe.pb.h caffe/include/caffe/proto
--- scripts/default_modelsplitter.sh
+++ scripts/default_modelsplitter.sh
@@ -7,10 +7,16 @@ then
  fi
 
 
-ARGS="./networks/AgeNet/age_net.caffemodel
-./networks/GenderNet/gender_net.caffemodel
+ARGS="
+./networks/AgeNet/age_net.caffemodel
+./networks/EmotionNet/EmotiW_VGG_S.caffemodel
 ./networks/FaceNet/face_full_conv.caffemodel
+./networks/GenderNet/gender_net.caffemodel
+./networks/MemNet/memnet.caffemodel
+./networks/ObjectNet/VGG_ILSVRC_19_layers.caffemodel
+./networks/SceneNet/places205CNN_iter_300000_upgraded.caffemodel
 ./networks/SoS/AlexNet_SalObjSub.caffemodel
-./networks/SoS_GoogleNet/GoogleNet_SOS.caffemodel"
+./networks/SoS_GoogleNet/GoogleNet_SOS.caffemodel
+./networks/TinyYolo/tiny_yolo.caffemodel"
 
 ${PREFIX}/ModelSplitter ${ARGS}
