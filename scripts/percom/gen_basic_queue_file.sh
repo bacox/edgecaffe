b3fde1aeec0ebd5a86082613942a4896bc1f4bc1
bacox <bartcox93@gmail.com>
2021-02-10 03:18:47 +0100

    Fix typo

--- scripts/percom/gen_basic_queue_file.sh
+++ scripts/percom/gen_basic_queue_file.sh
@@ -3,5 +3,5 @@
 FILE=example.tmp.txt
 echo "" > $FILE
 echo "bulk_example experiments/example/configs/2G/bulk-example.yaml" >> $FILE
-echo "deepeye_example experiments/example/configs/2G/deepeye_example.yaml" >> $FILE
+echo "deepeye_example experiments/example/configs/2G/deepeye-example.yaml" >> $FILE
 echo "masa_example experiments/example/configs/2G/masa-example.yaml" >> $FILE
